dc213994e65acc29506d0a9d9f38ea56294a970a
Samuel-prog182 <samuellb.o.w.182@hotmail.com>
2021-11-18 17:52:32 +0000

    Nota Aluno

---
+++ Teste.java
@@ -0,0 +1,17 @@
+public class Teste {
+    
+    public static void main(String[] args) {
+        
+        Aluno aluno=new Aluno("java",7.0,8.0,9.0);
+        Aluno aluno2=new Aluno(7,5,3);
+        Aluno aluno3=new Aluno();
+      
+         if(aluno.aprovado()){
+
+        }
+        else{
+
+        }
+
+    }
+}
